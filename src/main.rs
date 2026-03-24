0c1fd517ed5f066ae12aed380d0513bded897c4b
Nils Martel <nilsmartel@yahoo.de>
2026-03-24 10:07:34 +0100

    Read sample json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::str::FromStr;
+
+use serde_json::Value;
+
 fn main() {
-    println!("Hello, world!");
+    let samplejson = include_str!("./sample-json.json");
+    let value = Value::from_str(samplejson).expect("parse json");
+    println!("{}", value);
 }
