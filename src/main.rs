4e7ce23b1d8cbac1856c4173c4778cd3698d570a
Nils Martel <nilsmartel@yahoo.de>
2026-03-24 10:07:53 +0100

    Fix filepath

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::str::FromStr;
 use serde_json::Value;
 
 fn main() {
-    let samplejson = include_str!("./sample-json.json");
+    let samplejson = include_str!("../sample-json.json");
     let value = Value::from_str(samplejson).expect("parse json");
     println!("{}", value);
 }
