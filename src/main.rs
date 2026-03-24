da14c82f0aa5c8c6e1fc560a22b3e805a2f7b60a
Nils Martel <nilsmartel@yahoo.de>
2026-03-24 10:11:47 +0100

    Write xilem boilerplate

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,15 @@ use std::str::FromStr;
 
 use serde_json::Value;
 
+use xilem::{WindowOptions, Xilem, core::View};
+
 fn main() {
     let samplejson = include_str!("../sample-json.json");
     let value = Value::from_str(samplejson).expect("parse json");
-    println!("{}", value);
+
+    Xilem::new_simple(value, logic, WindowOptions::new("json viewer"));
 }
+
+impl View for Value {}
+
+fn logic(value: &mut Value) {}
